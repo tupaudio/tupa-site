8d8ccc03cad58425b67536f39e2acb3be9577ef7
tupa <dafnhe.thor@msn.com>
2026-07-20 10:54:05 -0300

    Atualização de performance e correção de erro 1101, mais uma tentativa, espero que de certo!!!

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,17 +1,3 @@
-// open-next.config.ts
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  framework: {
-    name: "next",
-    version: "16.2.10",
-  },
-  runtime: "nodejs",
-  cloudflare: {
-    compatibility_flags: ["nodejs_compat"],
-  },
-  // Configuração para evitar fs.readFileSync
-  build: {
-    command: "next build --webpack",
-  },
-});
+export default defineCloudflareConfig({});
